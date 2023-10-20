9a0248dc32d38d74bd05fc98a3afb41b8ea643bf
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-20 15:30:28 +0200

    feat: easter-eggs

--- function.js
+++ function.js
@@ -15,8 +15,8 @@ function play() {
 function restart() {
     const audio = document.getElementById('never');
     const play = document.querySelector('#play');
-    audio.play();
     audio.currentTime = 0
+    audio.play();
     play.classList.add('fa-circle-pause');
     play.classList.remove('fa-circle-play');
 }
@@ -37,4 +37,41 @@ function stop() {
     audio.currentTime = 0
     play.classList.add('fa-circle-play');
     play.classList.remove('fa-circle-pause');
+}
+
+function openFullscreen() {
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+function show() {
+    document.getElementById('bsod')
+        .style.display = "block";
+}
+
+function hide() {
+    document.getElementById('bsod')
+        .style.display = "none";
+}
+
+function trumpet() {
+    const audio = document.getElementById('sad-trumpet');
+    audio.play();
+    audio.volume = 0.3;
 }
