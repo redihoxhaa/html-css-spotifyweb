37e01a609d5972dc39b2e7f41e8d8bb7d6df0e72
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-20 12:03:22 +0200

    fix: inclusione jquery

--- function.js
+++ function.js
@@ -2,12 +2,12 @@ function play() {
     var audio = document.getElementById('audio');
     if (audio.paused) {
         audio.play();
-        $('#play').removeClass('fa-regular fa-circle-play')
-        $('#play').addClass('fa-regular fa-circle-pause')
+        $('#play').removeClass('fa-circle-play')
+        $('#play').addClass('fa-circle-pause')
     } else {
         audio.pause();
         audio.currentTime = 0
-        $('#play').addClass('fa-regular fa-circle-play')
-        $('#play').removeClass('fa-regular fa-circle-pause')
+        $('#play').addClass('fa-circle-play')
+        $('#play').removeClass('fa-circle-pause')
     }
 }
