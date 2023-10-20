affed07fa837921c46039520c459947047dc1cce
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-20 14:39:49 +0200

    feat: funzionalità aggiunte barra controlli audio

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 function play() {
-    const audio = document.getElementById('canzone');
+    const audio = document.getElementById('never');
     const play = document.querySelector('#play');
     if (audio.paused) {
         audio.play();
@@ -10,4 +10,31 @@ function play() {
         play.classList.add('fa-circle-play');
         play.classList.remove('fa-circle-pause');
     }
+}
+
+function restart() {
+    const audio = document.getElementById('never');
+    const play = document.querySelector('#play');
+    audio.play();
+    audio.currentTime = 0
+    play.classList.add('fa-circle-pause');
+    play.classList.remove('fa-circle-play');
+}
+
+function next() {
+    const audio = document.getElementById('volevi');
+    const play = document.querySelector('#play');
+    audio.play();
+    audio.volume = 0.2;
+    play.classList.add('fa-circle-pause');
+    play.classList.remove('fa-circle-play');
+}
+
+function stop() {
+    const audio = document.getElementById('never');
+    const play = document.querySelector('#play');
+    audio.pause();
+    audio.currentTime = 0
+    play.classList.add('fa-circle-play');
+    play.classList.remove('fa-circle-pause');
 }
