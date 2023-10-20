9a62b3f72fee9d14e3d5b4bdb48363d25b75de3b
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-20 11:43:21 +0200

    fix: fix js

--- function.js
+++ function.js
@@ -2,12 +2,12 @@ function play() {
     var audio = document.getElementById('audio');
     if (audio.paused) {
         audio.play();
-        $('#play').removeClass("fa-regular fa-circle-play")
-        $('#play').addClass("fa-regular fa-circle-pause")
+        $('#play').removeClass('fa-regular fa-circle-play')
+        $('#play').addClass('fa-regular fa-circle-pause')
     } else {
         audio.pause();
         audio.currentTime = 0
-        $('#play').addClass("fa-regular fa-circle-play")
-        $('#play').removeClass("fa-regular fa-circle-pause")
+        $('#play').addClass('fa-regular fa-circle-play')
+        $('#play').removeClass('fa-regular fa-circle-pause')
     }
 }
