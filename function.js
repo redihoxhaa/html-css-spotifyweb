33f7efd43bcdd2ecf2626a1fa4b22feb6c340312
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-20 11:05:58 +0200

    feat: inclusione js e fix uppercase

---
+++ function.js
@@ -0,0 +1,13 @@
+function play() {
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass("fa-regular fa-circle-play")
+        $('#play').addClass("fa-regular fa-circle-pause")
+    } else {
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass("fa-regular fa-circle-play")
+        $('#play').removeClass("fa-regular fa-circle-pause")
+    }
+}
